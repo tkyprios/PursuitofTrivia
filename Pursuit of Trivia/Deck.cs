f79a73e9345573a23920fd1a1482a7ea3e015b20
tkyprios <114837393+tkyprios@users.noreply.github.com>
2025-09-15 14:07:30 +1000

    Create a method for seeding initial hard-coded questions, providing fallback choices for cards. Begin creation of CreateCard() with addition of custom Shuffle() extension
    custom Shuffle() extension will allow the shuffling of an arbitrary type (e.g. card, string etc.)

--- Pursuit of Trivia/Deck.cs
+++ Pursuit of Trivia/Deck.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
+using Pursuit_of_Trivia.Extensions;
 
 namespace Pursuit_of_Trivia
 
@@ -16,7 +19,8 @@ namespace Pursuit_of_Trivia
     {
         private Dictionary<Category, List<Card>> QuestionDecks;
 
-        private Dictionary<Category, List<string>> FallbackAnswers;
+        private Dictionary<Category, List<string>> FallbackChoices;
+
 
         /// <summary>
         /// Constructor for Deck class that initializes a master dictionary to hold all questions categorized by their respective categories.
@@ -32,18 +36,54 @@ namespace Pursuit_of_Trivia
                 QuestionDecks[category] = new List<Card>();
             }
 
-            InitializeFallbackAnswers();
+            SeedInitialQuestions();
+
+            InitializeFallbackChoices();
         }
 
 
-        //private Card CreateCard(Category questionCategory)
-        //{
+        private Card CreateCard(Category questionCategory, string questionText, string correctAnswer)
+        {
 
-        //    // Logic for finding all answers from same category
+            // Logic for finding all answers from same category
+            IEnumerable<Card> existingCategoryCards = QuestionDecks[questionCategory];
+            List<string> newCardChoices = new List<string>();
 
-        //    // Logic for getting the question and correct answer
-        //    return new Card(questionCategory, questionText, choices, correctAnswer);
-        //}
+            newCardChoices.Add(correctAnswer); // first choice to be added
+
+            foreach (var selectedCard in existingCategoryCards)
+            {
+                if (newCardChoices.Count == 4) break;
+                foreach (string choice in selectedCard.Choices)
+                {
+                    if (newCardChoices.Contains(choice)) continue;
+                    newCardChoices.Add(choice);
+
+                }
+            }
+
+            // Use Fallback answers if not enough
+            if (newCardChoices.Count < 4)
+            {
+                IEnumerable<String> categoryFallbackChoices = FallbackChoices[questionCategory].Shuffle();
+
+                foreach (string choice in categoryFallbackChoices)
+                {
+                    if (newCardChoices.Count == 4) break;
+                    if (!newCardChoices.Contains(choice))
+                        newCardChoices.Add(choice);
+                    }
+                    
+             }
+
+            // Shuffle the final choices so the correct answer isn't always first. Use 'Shuffle' method created in PursuitOfTrivia.Extensions.
+            newCardChoices = newCardChoices.Shuffle();
+
+            //@TODO see what AI thinks of the current setup. Is there a better way to structure?
+
+            // Logic for getting the question and correct answer
+            return new Card(questionCategory, questionText, newCardChoices, correctAnswer);
+        }
 
         /// <summary>
         /// A void method to provide a dictionary of choices/answers when needed.
@@ -52,10 +92,10 @@ namespace Pursuit_of_Trivia
         ///           Consider the scenario: Given the Deck class uses answers from other questions of the same category 
         ///           to fill the 'choices' on a card, the first few cards will not have enough other cards to source choices from.
         ///           To fix this, we provide generic answers relevant to each category that can be used as 'fallback'.
-        /// /remarks>
-        private void InitializeFallbackAnswers()
+        /// </remarks>
+        private void InitializeFallbackChoices()
         {
-            FallbackAnswers = new Dictionary<Category, List<string>>
+            FallbackChoices = new Dictionary<Category, List<string>>
             {
                 [Category.Characters] = new List<string>
                 {
@@ -99,9 +139,131 @@ namespace Pursuit_of_Trivia
 
         }
 
+
+        /// <summary>
+        /// Populates the question decks with an initial set of trivia questions categorized by topic. Hard coded in this version of the game.
+        /// </summary>
+        /// <remarks>This method seeds the <see cref="QuestionDecks"/> collection with predefined trivia
+        /// questions grouped into their respective categories. This ensures there are some questions, should the dynamic 
+        /// system fail. </remarks>
         private void SeedInitialQuestions()
         {
-            // @TODO add 5 hardcoded questions per category
+            QuestionDecks[Category.Characters].Add(
+                new Card(
+                    Category.Characters,
+                    "Who trained Count Dooku before he became a Sith Lord?",
+                    new List<string> { "Mace Windu", "Qui-Gon Jinn", "Yoda", "Sifo-Dyas" },
+                    "Yoda"
+                ));
+            QuestionDecks[Category.Characters].Add(
+                new Card(
+                    Category.Characters,
+                    "What is Admiral Ackbar's species?",
+                    new List<string> { "Quarren", "Mon Calamari", "Ithorian", "Rodian" },
+                    "Mon Calamari"
+                ));
+            QuestionDecks[Category.Characters].Add(
+                new Card(
+                    Category.Characters,
+                    "Which Jedi discovered Ahsoka Tano as a Force-sensitive child?",
+                    new List<string> { "Plo Koon", "Shaak Ti", "Yoda", "Anakin Skywalker" },
+                    "Plo Koon"
+                ));
+
+
+            QuestionDecks[Category.PlanetsAndLocations].Add(
+                new Card(
+                    Category.PlanetsAndLocations,
+                    "On what planet did Anakin and Obi-Wan duel at the end of Revenge of the Sith?",
+                    new List<string> { "Mustafar", "Naboo", "Utapau", "Geonosis" },
+                    "Mustafar"
+                ));
+            QuestionDecks[Category.PlanetsAndLocations].Add(
+                new Card(
+                    Category.PlanetsAndLocations,
+                    "What planet is the homeworld of the Wookies?",
+                    new List<string> { "Endor", "Felucia", "Kashyyyk", "Dantooine" },
+                    "Kashyyyk"
+                ));
+            QuestionDecks[Category.PlanetsAndLocations].Add(
+                new Card(
+                    Category.PlanetsAndLocations,
+                    "Where is the Jedi Temple located in the prequel trilogy?",
+                    new List<string> { "Ord Mantell", "Jakku", "Malastare", "Coruscant" },
+                    "Coruscant"
+                ));
+
+
+            QuestionDecks[Category.ShipsVehiclesAndTech].Add(
+                new Card(
+                    Category.ShipsVehiclesAndTech,
+                    "What class of ship is the Millennium Falcon?",
+                    new List<string> { "YT-1300 light freighter", "Firespray-31", "Corellian Corvette", "CR90 transport" },
+                    "YT-1300 light freighter"
+                ));
+            QuestionDecks[Category.ShipsVehiclesAndTech].Add(
+                new Card(
+                    Category.ShipsVehiclesAndTech,
+                    "What is the name of Darth Vader's personal TIE fighter variant?",
+                    new List<string> { "TIE Bomber", "TIE Advanced x1", "TIE Defender", "TIE Interceptor" },
+                    "TIE Advanced x1"
+                ));
+            QuestionDecks[Category.ShipsVehiclesAndTech].Add(
+                new Card(
+                    Category.ShipsVehiclesAndTech,
+                    "Which Rebel starfighter is known for its split S-foils?",
+                    new List<string> { "A-Wing", "B-Wing", "X-Wing", "Y-Wing" },
+                    "X-Wing"
+                ));
+
+
+            QuestionDecks[Category.EventsAndBattles].Add(
+                new Card(
+                    Category.EventsAndBattles,
+                    "What event sparked the beginning of the Clone Wars?",
+                    new List<string> { "Siege of Mandalore", "Battle of Geonosis", "Battle of Naboo", "Attack on Kamino" },
+                    "Battle of Geonosis"
+                ));
+            QuestionDecks[Category.EventsAndBattles].Add(
+                new Card(
+                    Category.EventsAndBattles,
+                    "Who killed Darth Maul in their final duel?",
+                    new List<string> { "Qui-Gon Jinn", "Yoda", "Anakin Skywalker", "Obi-Wan Kenobi" },
+                    "Obi-Wan Kenobi"
+                ));
+            QuestionDecks[Category.EventsAndBattles].Add(
+                new Card(
+                    Category.EventsAndBattles,
+                    "Which battle led to the destruction of the second Death Star?",
+                    new List<string> { "Battle of Yavin", "Battle of Endor", "Battle of Scarif", "Battle of Jakku" },
+                    "Battle of Endor"
+                ));
+
+
+
+            QuestionDecks[Category.QuotesAndLore].Add(
+                new Card(
+                    Category.QuotesAndLore,
+                    "Who said I find your lack of faith disturbing?",
+                    new List<string> { "Emperor Palpatine", "Grand Moff Tarkin", "Darth Vader", "Count Dooku" },
+                    "Darth Vader"
+                ));
+            QuestionDecks[Category.QuotesAndLore].Add(
+                new Card(
+                    Category.QuotesAndLore,
+                    "According to Yoda, what does fear lead to?",
+                    new List<string> { "Suffering", "Hate", "Anger", "Darkness" },
+                    "Anger"
+                ));
+            QuestionDecks[Category.QuotesAndLore].Add(
+                new Card(
+                    Category.QuotesAndLore,
+                    "What were Obi-Wan Kenobi's final words to Darth Vader before his death?",
+                    new List<string> { "If you strike me down, I shall become more powerful than you can possibly imagine.", "You can't win, Darth.", "You'll never defeat me.", "The Force will guide me." },
+                    "If you strike me down, I shall become more powerful than you can possibly imagine."
+                ));
+            
         }
+
     }
 }
---
+++ Pursuit of Trivia/Extensions/CollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pursuit_of_Trivia.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for collection operations.
+    /// </summary>
+    internal static class CollectionExtensions
+    {
+        // Use the same Random instance for all shuffling to avoid timing issues
+        private static readonly Random _random = new Random();
+
+        /// <summary>
+        /// Shuffles the elements of a collection using the Fisher-Yates algorithm.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="source">The source collection to shuffle.</param>
+        /// <returns>A new List containing the elements in random order.</returns>
+        public static List<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            // Convert to array for Fisher-Yates shuffle
+            var array = source.ToArray();
+            int n = array.Length;
+
+            // Fisher-Yates shuffle algorithm
+            while (n > 1)
+            {
+                n--;
+                int k = _random.Next(n + 1);
+                T temp = array[k];
+                array[k] = array[n];
+                array[n] = temp;
+            }
+
+            return array.ToList();
+        }
+    }
+}
